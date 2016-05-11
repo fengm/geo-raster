6dcd2f5e745b70bc3eeff595f638fab7897ff31e
Min Feng <feng.tank@gmail.com>
2016-05-11 22:06:12 +0000

    add cached s3 instance

--- lib/cache_mag.py
+++ lib/cache_mag.py
@@ -15,22 +15,62 @@ class cache_mag():
 		self._t = tag
 		self._d = config.get('conf', 'cache')
 
-	def path(self, lid):
+	def cached(self, key):
+		_f = self.path(key)
 		import os
-		return os.path.join(self._d, lid.tile, '%s_%s.tif' % (lid, self._t))
+		return os.path.exists(_f) and os.path.getsize(_f) > 0
 
-	def has_id(self, lid):
+	def path(self, key):
 		import os
-		return os.path.exists(self.path(lid))
+		return os.path.join(self._d, self._t, key)
 
-	def cache(self, lid, f):
-		_f = self.path(lid)
+	def get(self, key):
+		if not self.cached(key):
+			return None
+
+		return self.path(key)
+
+	def put(self, key, inp):
+		_f = self.path(key)
+
+		if self.cached(key):
+			return _f
 
 		import os
 		(lambda x: os.path.exists(x) or os.makedirs(x))(os.path.dirname(_f))
 
 		import shutil
-		shutil.copy(f, _f)
+		shutil.copy(inp, _f)
+
+		return _f
+
+class s3():
+	"""manage Landsat cache files"""
+
+	def __init__(self, bucket):
+		self._t = bucket
+		self._c = cache_mag(bucket)
+		self.bucket = self._bucket()
+
+	def _bucket(self):
+		import boto
+
+		_s3 = boto.connect_s3()
+		_bk = _s3.get_bucket(self._t)
+
+		return _bk
+
+	def get(self, key):
+		_f = self._c.path(key.key)
+
+		if self._c.cached(key.key):
+			return _f
+
+		import os
+		(lambda x: os.path.exists(x) or os.makedirs(x))(os.path.dirname(_f))
+
+		with open(_f, 'wb') as _fo:
+			self.bucket.get_key(key).get_contents_to_file(_fo)
 
 		return _f
 
