93b87af61d273aa909114c79f9bbc6731b9ba373
Min Feng <feng.tank@gmail.com>
2016-12-05 19:34:28 -0500

    add range function

--- mod/geo_grid_c/geo_grid_c.pyx
+++ mod/geo_grid_c/geo_grid_c.pyx
@@ -33,3 +33,24 @@ class neighbor:
 
 		return _var
 
+	def range(self, np.ndarray[np.float32_t, ndim=2] dat, dist):
+		cdef int _rows = dat.shape[0], _cols = dat.shape[1]
+		cdef int _rrr, _ccc
+		cdef int _row, _col
+		cdef float _v
+
+		cdef np.ndarray[np.float32_t, ndim=2] _var = np.zeros((_rows, _cols), dtype=np.float32)
+
+		for _row in xrange(_rows):
+			for _col in xrange(_cols):
+				_vs = []
+				for _rrr in xrange(max(0, _row - dist), min(_rows, _row + dist)):
+					for _ccc in xrange(max(0, _col - dist), min(_cols, _col + dist)):
+						_v = dat[_rrr, _ccc]
+						if _v not in _vs:
+							_vs.append(_v)
+
+				_var[_row, _col] = max(_vs) - min(_vs)
+
+		return _var
+
