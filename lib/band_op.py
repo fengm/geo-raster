a0c0619fcd1a4daa6f823019d9c6f32d371607b9
fengm <feng.tank@gmail.com>
2020-08-30 07:08:28 +0000

    support gray band

--- lib/band_op.py
+++ lib/band_op.py
@@ -4,6 +4,8 @@ Author: Min Feng
 Description: operate bands
 '''
 
+import logging
+
 def burn_band(bnd, clr_bnd, ref, clr_ref, offset=250):
     '''burn another band into the source band to create RGB image'''
     
@@ -19,8 +21,19 @@ def burn_band(bnd, clr_bnd, ref, clr_ref, offset=250):
     if bnd is None or _ref is None:
         return None
         
-    _bnd = bnd.colorize_rgba(clr_bnd)
-    _ref = _ref.colorize_rgba(clr_ref) 
+    _bnd = bnd if len(bnd.data.shape) > 2 else bnd.colorize_rgba(clr_bnd)
+    if _ref.color_table is None and clr_ref is None and _ref.pixel_type == ge.pixel_type():
+        _r = np.zeros((4, _bnd.height, _bnd.width), dtype=np.uint8)
+        for _b in range(3):
+            _r[_b, :, :] = _ref.data
+            
+        _r[3, :, :] = 255
+        if _ref.nodata is not None:
+            _r[3, :, :][_ref.data == _ref.nodata] = 0
+            
+        _ref = _ref.from_grid(_r)
+    else:
+        _ref = _ref.colorize_rgba(clr_ref) 
     
     _d = np.zeros((4, _bnd.height, _bnd.width), dtype=np.int16)
     for _n in range(3):
@@ -35,10 +48,14 @@ def burn_band(bnd, clr_bnd, ref, clr_ref, offset=250):
         
         _d[_n, :, :] = _o.astype(np.uint8)
         
-    _d[3, :, :] = _bnd.data[3, :, :]
-    return _bnd.from_grid(_d)
+    _a = _bnd.data[3, :, :]
+    _a[_ref.data[3, :, :] == 0] = 0
+    _d[3, :, :] = _a
     
-def burn_transparency(bnd, clr_bnd, ref, v_min, v_max, offset=250):
+    _out = _bnd.from_grid(_d.astype(np.uint8))
+    return _out
+    
+def burn_transparency(bnd, clr_bnd, ref, v_min, v_max):
     '''burn another band into the source band as transparency to create RGB image'''
     
     from gio import geo_raster as ge
@@ -53,7 +70,7 @@ def burn_transparency(bnd, clr_bnd, ref, v_min, v_max, offset=250):
     if bnd is None or _ref is None:
         return None
         
-    _bnd = bnd.colorize_rgba(clr_bnd)
+    _bnd = bnd if len(bnd.data.shape) > 2 else bnd.colorize_rgba(clr_bnd)
     
     _d = (_ref.data - v_min) * 255 / (v_max - v_min)
     _d[_d < 0] = 0
