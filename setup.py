f85be32dc1c85b7d9f59ad5339e328499e542a5f
Min Feng <feng.tank@gmail.com>
2023-01-11 18:47:11 +0000

    ignore files and folders start with .

--- setup.py
+++ setup.py
@@ -5,29 +5,48 @@ from setuptools.command.build_ext import build_ext
 import os
 import numpy
 
+def _walk_files(d, fs):
+    for _f in os.listdir(d):
+        if _f.startswith('.'):
+            continue
+            
+        _a = os.path.join(d, _f)
+        if os.path.isdir(_a):
+            _walk_files(_a, fs)
+            continue
+            
+        fs.append((d, _f))
+        
+def walk_files(d):
+    _fs = []
+    _walk_files(d, _fs)
+    return _fs
+
 _ms = []
-for _root, _dirs, _files in os.walk('mod'):
-    for _file in _files:
-        if _file.endswith('.pyx'):
-            _f, _e = os.path.splitext(_file)
 
-            _n = _f
-            if _n.endswith('_c'):
-                _n = _n[:-2]
+for _root, _file in walk_files('mod'):
+    if not _file.endswith('.pyx'):
+        continue
+        
+    _f, _e = os.path.splitext(_file)
 
-            _ms.append(setuptools.Extension("gio.%s" % _n, [os.path.join(_root, _file)],
-                #extra_compile_args=["-O3", "-ffast-math","-funroll-loops"],
-                define_macros=[("NPY_NO_DEPRECATED_API", None)], 
-                include_dirs=[numpy.get_include()]))
+    _n = _f
+    if _n.endswith('_c'):
+        _n = _n[:-2]
+
+    _ms.append(setuptools.Extension("gio.%s" % _n, [os.path.join(_root, _file)],
+        #extra_compile_args=["-O3", "-ffast-math","-funroll-loops"],
+        define_macros=[("NPY_NO_DEPRECATED_API", None)], 
+        include_dirs=[numpy.get_include()]))
 
 _ss = []
-for _root, _dirs, _files in os.walk('util'):
-    for _file in _files:
-        if not _file.endswith('.py'):
-            continue
 
-        _f = os.path.join(_root, _file)
-        _ss.append(_f)
+for _root, _file in walk_files('util'):
+    if not _file.endswith('.py'):
+        continue
+
+    _f = os.path.join(_root, _file)
+    _ss.append(_f)
 
 _exts = []
 if _ms:
